a7f8272cd40d0250e1a42d58630588e6b18fe399
sashik <mrnormalnybog@gmail.com>
2024-01-18 17:57:24 +1000

    Ready

--- main.cpp
+++ main.cpp
@@ -1,157 +1,402 @@
-#include <iostream>
-#include <string>
+#include<iostream>
+#include <vector>
+
 using namespace std;
 
-// Базовый класс Human, представляющий человека с именем и возрастом
-class Human {
-private:
-    string name;
-    int age;
-
+class Pizza {
 public:
-    // Конструктор класса Human для полей
-    Human(string n, int a) : name(n), age(a) {}
-
-    // Getter и Setter для свойства name
-    string getName() const {
-        return name;
+    virtual double GetCost() {
+        return 0.0;
     }
 
-    void setName(const string& n) {
-        name = n;
+    virtual void Price() {
+        cout << "Не знаю цену для этой пиццы...\n";
     }
 
-    // Getter и Setter для свойства age
-    int getAge() const {
-        return age;
+    virtual void Print_Info() {
+        cout << "Информация о вашей пицце:\n";
     }
 
-    void setAge(int a) {
-        age = a;
+    virtual void SetSize(int value) {
+        cout << "Вы хотите пиццу размером " << value << " сантиметров?\n Хорошо...\n";
+    }
+
+    virtual void AddCheese() {
+        cout << "Почему вы хотите добавить сыр к пицце?...\n";
+    }
+
+    virtual void SaltPizza() {
+        cout << "Почему вы хотите добавить соль к пицце?...\n";
     }
 };
 
-// Класс Student, наследующийся от Human
-class Student : public Human {
+class Pepperoni : public Pizza {
 private:
-    string university;
+    string Name;
+    string Info;
+    unsigned int Size;
+    double cost;
+    int SaltValue;
+    int CheeseValue;
 
 public:
-    // Конструктор класса Student для полей Human и university
-    Student(string n, int a, string s) : Human(n, a), university(s) {}
-
-    // Getter и Setter для свойства university
-    string getUniversity() const {
-        return university;
+    Pepperoni() {
+        Name = "Пепперони";
+        Info = "Вкусная пицца. Хороший выбор!";
+        Size = 0;
+        cost = 0;
+        SaltValue = 0;
+        CheeseValue = 0;
     }
 
-    void setUniversity(const string& s) {
-        university = s;
+    void Price() override {
+        switch (this->Size) {
+            case 25:
+                this->cost += 100 + this->CheeseValue * 20 + this->SaltValue * 0.5;
+                break;
+            case 30:
+                this->cost += 100 + this->CheeseValue * 20 + this->SaltValue * 0.5;
+                break;
+            case 35:
+                this->cost += 100 + this->CheeseValue * 20 + this->SaltValue * 0.5;
+                break;
+            case 40:
+                this->cost += 100 + this->CheeseValue * 20 + this->SaltValue * 0.5;
+                break;
+            default:
+                cout << "Выберите размер пиццы!\n";
+                break;
+        }
     }
 
-    virtual void goesToSchool() {
-        cout << getName() << " учится в " << getUniversity() << endl;
+    void Print_Info() override {
+        cout << "Название: " << this->Name << endl;
+        cout << "Информация: " << this->Info << endl;
+        cout << "Размер: " << this->Size << endl;
+        cout << "Добавка сыра: " << this->CheeseValue << endl;
+        cout << "Добавка соли: " << this->SaltValue << endl;
+        cout << "Цена: " << this->cost << endl;
+    }
+
+    double GetCost() override {
+        return this->cost;
+    }
+
+    void AddCheese() override {
+        unsigned int cheeseValue = 0;
+        cout << "Сколько добавить сыра?\n";
+        cin >> cheeseValue;
+        if (cheeseValue > 0)
+            this->CheeseValue = cheeseValue;
+    }
+
+    void SaltPizza() override {
+        unsigned int saltValue = 0;
+        cout << "Сколько добавить соли?\n";
+        cin >> saltValue;
+        if (saltValue > 0)
+            this->SaltValue = saltValue;
+    }
+
+    void SetSize(int value) override {
+        switch (value) {
+            case 1:
+                this->Size = 25;
+                break;
+            case 2:
+                this->Size = 30;
+                break;
+            case 3:
+                this->Size = 35;
+                break;
+            case 4:
+                this->Size = 40;
+                break;
+            default:
+                cout << "Этот размер не определен!\n";
+                break;
+        }
     }
 };
 
-
-// Класс Teacher, наследующийся от Human
-class Teacher : public Human {
+class Margarita : public Pizza {
 private:
-    string subject;
+    string Name;
+    string Info;
+    unsigned int Size;
+    double cost;
+    int SaltValue;
+    int CheeseValue;
 
 public:
-    // Конструктор класса Teacher для полей Human и subject
-    Teacher(string n, int a, string s) : Human(n, a), subject(s) {}
-
-    // Getter и Setter для свойства subject
-    string getSubject() const {
-        return subject;
+    Margarita() {
+        Name = "Маргарита";
+        Info = "Классическая пицца с томатом и сыром";
+        Size = 0;
+        cost = 0;
+        SaltValue = 0;
+        CheeseValue = 0;
     }
 
-    void setSubject(const string& s) {
-        subject = s;
+    void Price() override {
+        switch (this->Size) {
+            case 25:
+                this->cost += 120 + this->CheeseValue * 25 + this->SaltValue * 0.6;
+                break;
+            case 30:
+                this->cost += 120 + this->CheeseValue * 25 + this->SaltValue * 0.6;
+                break;
+            case 35:
+                this->cost += 120 + this->CheeseValue * 25 + this->SaltValue * 0.6;
+                break;
+            case 40:
+                this->cost += 120 + this->CheeseValue * 25 + this->SaltValue * 0.6;
+                break;
+            default:
+                cout << "Выберите размер пиццы!\n";
+                break;
+        }
+    }
+
+    void Print_Info() override {
+        cout << "Название: " << this->Name << endl;
+        cout << "Информация: " << this->Info << endl;
+        cout << "Размер: " << this->Size << endl;
+        cout << "Добавка сыра: " << this->CheeseValue << endl;
+        cout << "Добавка соли: " << this->SaltValue << endl;
+        cout << "Цена: " << this->cost << endl;
+    }
+
+    double GetCost() override {
+        return this->cost;
+    }
+
+    void AddCheese() override {
+        unsigned int cheeseValue = 0;
+        cout << "Сколько добавить сыра?\n";
+        cin >> cheeseValue;
+        if (cheeseValue > 0)
+            this->CheeseValue = cheeseValue;
+    }
+
+    void SaltPizza() override {
+        unsigned int saltValue = 0;
+        cout << "Сколько добавить соли?\n";
+        cin >> saltValue;
+        if (saltValue > 0)
+            this->SaltValue = saltValue;
+    }
+
+    void SetSize(int value) override {
+        switch (value) {
+            case 1:
+                this->Size = 25;
+                break;
+            case 2:
+                this->Size = 30;
+                break;
+            case 3:
+                this->Size = 35;
+                break;
+            case 4:
+                this->Size = 40;
+                break;
+            default:
+                cout << "Этот размер не определен!\n";
+                break;
+        }
     }
 };
 
-// Класс Coresh, наследующийся от Human
-class Coresh : public Human {
+class Hawaiian : public Pizza {
 private:
-    string hobby;
+    string Name;
+    string Info;
+    unsigned int Size;
+    double cost;
+    int SaltValue;
+    int CheeseValue;
 
 public:
-    // Конструктор класса Coresh для полей Human и hobby
-    Coresh(string n, int a, string h) : Human(n, a), hobby(h) {}
-
-    // Getter и Setter для свойства hobby
-    string getHobby() const {
-        return hobby;
+    Hawaiian() {
+        Name = "Гавайская";
+        Info = "Пицца с ветчиной и ананасами";
+        Size = 0;
+        cost = 0;
+        SaltValue = 0;
+        CheeseValue = 0;
     }
 
-    void setHobby(const string& h) {
-        hobby = h;
+    void Price() override {
+        switch (this->Size) {
+            case 25:
+                this->cost += 140 + this->CheeseValue * 30 + this->SaltValue * 0.8;
+                break;
+            case 30:
+                this->cost += 140 + this->CheeseValue * 30 + this->SaltValue * 0.8;
+                break;
+            case 35:
+                this->cost += 140 + this->CheeseValue * 30 + this->SaltValue * 0.8;
+                break;
+            case 40:
+                this->cost += 140 + this->CheeseValue * 30 + this->SaltValue * 0.8;
+                break;
+            default:
+                cout << "Выберите размер пиццы!\n";
+                break;
+        }
+    }
+
+    void Print_Info() override {
+        cout << "Название: " << this->Name << endl;
+        cout << "Информация: " << this->Info << endl;
+        cout << "Размер: " << this->Size << endl;
+        cout << "Добавка сыра: " << this->CheeseValue << endl;
+        cout << "Добавка соли: " << this->SaltValue << endl;
+        cout << "Цена: " << this->cost << endl;
+    }
+
+    double GetCost() override {
+        return this->cost;
+    }
+
+    void AddCheese() override {
+        unsigned int cheeseValue = 0;
+        cout << "Сколько добавить сыра?\n";
+        cin >> cheeseValue;
+        if (cheeseValue > 0)
+            this->CheeseValue = cheeseValue;
+    }
+
+    void SaltPizza() override {
+        unsigned int saltValue = 0;
+        cout << "Сколько добавить соли?\n";
+        cin >> saltValue;
+        if (saltValue > 0)
+            this->SaltValue = saltValue;
+    }
+
+    void SetSize(int value) override {
+        switch (value) {
+            case 1:
+                this->Size = 25;
+                break;
+            case 2:
+                this->Size = 30;
+                break;
+            case 3:
+                this->Size = 35;
+                break;
+            case 4:
+                this->Size = 40;
+                break;
+            default:
+                cout << "Этот размер не определен!\n";
+                break;
+        }
     }
 };
 
-class Person {
+class PizzeriaWindow {
 private:
-    string character;
+    vector<Pizza*> OrderList;
 
 public:
-    Person(string c) : character(c) {}
-
-    string getCharacter() {
-        return character;
+    void PrintMenu() {
+        int N = 0;
+        cout << "Меню заказа\nВыберите действие:\n";
+        cout << "1) Добавить пиццу в заказ\n";
+        cout << "2) Вывести заказ\n";
+        cout << "3) Подтвердить и оплатить\n";
+        cout << "===> ";
+        cin >> N;
+        cout << endl;
+        switch (N) {
+            case 1:
+                ChoosePizza();
+                break;
+            case 2:
+                PrintOrder();
+                PrintMenu();
+                break;
+            case 3:
+                PrintOrder();
+                break;
+            default:
+                cout << "Выберите правильное действие!\n\n";
+                PrintMenu();
+        }
     }
 
-    void setCharacter(string _character) {
-        character = _character;
+    void ChoosePizza() {
+        int N = 0;
+        Pizza* pizza = nullptr;
+        cout << "Выберите пиццу:\n";
+        cout << "1) Пепперони\n";
+        cout << "2) Маргарита\n";
+        cout << "3) Гавайская\n";
+        cout << "4) Вернуться в меню\n";
+        cout << "===> ";
+        cin >> N;
+        cout << endl;
+        switch (N) {
+            case 1:
+                pizza = new Pepperoni();
+                break;
+            case 2:
+                pizza = new Margarita();
+                break;
+            case 3:
+                pizza = new Hawaiian();
+                break;
+            case 4:
+                PrintMenu();
+        }
+
+        cout << "Выберите размер пиццы:\n";
+        cout << "1) 25\n";
+        cout << "2) 30\n";
+        cout << "3) 35\n";
+        cout << "4) 40\n";
+        cout << "===> ";
+        cin >> N;
+        cout << endl;
+        if (N > 0 && N < 5) {
+            pizza->SetSize(N);
+            pizza->AddCheese();
+            pizza->SaltPizza();
+            pizza->Price();
+            this->OrderList.push_back(pizza);
+            cout << endl;
+            PrintMenu();
+        } else {
+            cout << "Неправильный параметр! Попробуйте снова...";
+            PrintMenu();
+        }
     }
 
-    virtual void doHobby() = 0;
-};
-
-// Класс WhistleMan, наследующийся от Student и Person
-class WhistleMan : public Student, public Person {
-public:
-    // Конструктор класса whistleMan для полей Student, Person
-    WhistleMan(string n, int a, string s, string c) : Student(n, a, s), Person(c) {}
-
-    void fighteverybody() {
-        cout << getName() << " дерётся со всеми\n";
+    void PrintOrder() {
+        int i = 1;
+        for (Pizza* pizza : this->OrderList) {
+            cout << "Позиция " << i << "\n\n";
+            pizza->Print_Info();
+            cout << endl;
+            i++;
+        }
+        PrintTotalCost();
     }
 
-    void doHobby() override {
-        cout << getName() << " играет в баскетболл\n";
-    }
-
-    void goesToSchool() override {
-        cout << "не любит ходить в вузик\n";
+    void PrintTotalCost() {
+        double TotalCost = 0.0;
+        for (Pizza* pizza : this->OrderList) {
+            TotalCost += pizza->GetCost();
+        }
+        cout << "Итоговая стоимость = " << TotalCost << endl;
     }
 };
 
 int main() {
-    // Создание объектов различных классов
-    Human person("Лешик", 19);
-    Student student("Сашик", 19, "ДВФУ");
-    Teacher teacher("МАО", 35, "матанализ");
-    Coresh coresh("Гриша", 19, "математикой");
-
-    // Вывод свойств объектов различных классов
-    cout << "Человек: " << person.getName() << ", возраст: " << person.getAge() << endl;
-    cout << "Студент: " << student.getName() << ", возраст: " << student.getAge()
-         << ", Университет: " << student.getUniversity() << endl;
-    cout << "Преподаватель: " << teacher.getName() << ", возраст: " << teacher.getAge()
-         << ", Subject: " << teacher.getSubject() << endl;
-    cout << "Кореш: " << coresh.getName() << ", возраст: " << coresh.getAge()
-         << ", любит заниматься: " << coresh.getHobby() << endl;
-
-    // Создание объекта whistleMan и вывод его свойств
-    WhistleMan whistleMan("Антон", 19, "ИМКТ", "куряга");
-    cout << whistleMan.getName() << " - " << whistleMan.getCharacter() << endl;
-    whistleMan.goesToSchool();
-    whistleMan.fighteverybody();
-    whistleMan.doHobby();
-
+    PizzeriaWindow window;
+    window.PrintMenu();
     return 0;
 }
